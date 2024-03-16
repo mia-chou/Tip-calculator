54ff2472a6377c095fe6d4cfa73d2dc5191daeeb
mia-chou <mia.fy.chou@hotmail.com>
2024-03-16 15:08:36 +0000

    Added CSS styling

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 function App() {
   return (
     <div>
-      <label htmlFor="">Bill Total</label>
+      <label>Bill Total</label>
+      <input type="text" placeholder={'0.00'}/>
+      <label>Tip</label>
       <input type="text" placeholder={'0.00'}/>
     </div>
   );
