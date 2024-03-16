a56ac6afc297ea42039f8a20ce3aa809780c932f
mia-chou <mia.fy.chou@hotmail.com>
2024-03-16 14:56:21 +0000

    Added form

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <label htmlFor="">Bill Total</label>
+      <input type="text" placeholder={'0.00'}/>
+    </div>
+  );
+}
+
+export default App;
